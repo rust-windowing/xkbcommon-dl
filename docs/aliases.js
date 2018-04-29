81479450407f85661cf775d89b387d8c17bdddd6
Francesca Frangipane <francesca@comfysoft.net>
2018-04-29 20:17:53 +0000

    Updated docs for 5b11331

---
+++ docs/aliases.js
@@ -0,0 +1,8 @@
+var ALIASES = {};
+ALIASES['bitflags'] = {};
+ALIASES['dlib'] = {};
+ALIASES['lazy_static'] = {};
+ALIASES['libc'] = {};
+ALIASES['libloading'] = {};
+ALIASES['x11_dl'] = {};
+ALIASES['xkbcommon_dl'] = {};
--- docs/implementors/core/cmp/trait.Eq.js
+++ docs/implementors/core/cmp/trait.Eq.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
-implementors["x11_dl"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"x11_dl/error/enum.OpenErrorKind.html\" title=\"enum x11_dl::error::OpenErrorKind\">OpenErrorKind</a>",];
-implementors["xkbcommon_dl"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"xkbcommon_dl/struct.xkb_state_component.html\" title=\"struct xkbcommon_dl::xkb_state_component\">xkb_state_component</a>",];
+implementors["x11_dl"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"x11_dl/error/enum.OpenErrorKind.html\" title=\"enum x11_dl::error::OpenErrorKind\">OpenErrorKind</a>",synthetic:false,types:["x11_dl::error::OpenErrorKind"]},];
+implementors["xkbcommon_dl"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"xkbcommon_dl/struct.xkb_state_component.html\" title=\"struct xkbcommon_dl::xkb_state_component\">xkb_state_component</a>",synthetic:false,types:["xkbcommon_dl::xkb_state_component"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
