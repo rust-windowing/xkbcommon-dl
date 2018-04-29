85121de3339c7dc4c41f807bf5aaa8d4a19e74e1
Francesca Frangipane <francesca@comfysoft.net>
2018-04-29 20:19:16 +0000

    Updated docs for be7f612

--- docs/implementors/std/error/trait.Error.js
+++ docs/implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["x11_dl"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"x11_dl/error/struct.OpenError.html\" title=\"struct x11_dl::error::OpenError\">OpenError</a>",];
+implementors["x11_dl"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"x11_dl/error/struct.OpenError.html\" title=\"struct x11_dl::error::OpenError\">OpenError</a>",synthetic:false,types:["x11_dl::error::OpenError"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
