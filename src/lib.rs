a5ce414955f3185ba573d3f55bdb3d4f20a6bd95
John Nunley <jtnunley01@gmail.com>
2023-06-27 13:17:51 -0700

    Use xkeysym instead of manual keysyms

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
 #![allow(dead_code, non_camel_case_types)]
+#![cfg_attr(rustfmt, rustfmt_skip)]
+
+extern crate dlib;
+extern crate bitflags;
+extern crate xkeysym;
+
+#[doc(inline)]
+pub use xkeysym::key as keysyms;
 
 use std::os::raw::{c_char, c_int, c_uint, c_void};
 
@@ -6,8 +14,6 @@ use bitflags::bitflags;
 use dlib::dlopen_external_library;
 use log::info;
 use once_cell::sync::OnceCell;
-
-pub mod keysyms;
 
 #[cfg(feature = "x11")]
 pub mod x11;
